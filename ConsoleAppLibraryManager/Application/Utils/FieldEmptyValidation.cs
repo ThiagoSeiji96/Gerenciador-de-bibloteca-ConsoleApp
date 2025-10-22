425575597f4349b95cdb4aa8a899d1c84b26426d
Thiago Seiji Imai Rozner <thiago.rozner@viveo.com.br>
2025-10-22 00:46:10 -0300

    created generic fieldValidation

---
+++ ConsoleAppLibraryManager/Application/Utils/FieldEmptyValidation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleAppLibraryManager.Application.Utils
+{
+    public static class FieldEmptyValidation
+    {
+        public static string FieldValidation(string fieldName, string entityName)
+        {
+            string optionSelected = string.Empty;
+            string fieldValue = string.Empty;
+
+            while (string.IsNullOrEmpty(fieldValue)){
+
+                Console.WriteLine($"{fieldName} cannot be null or empty.");
+                Console.WriteLine($"Please, enter the {entityName} {fieldName}:");
+                fieldValue = Console.ReadLine()!;
+
+                Console.WriteLine($"{fieldName} cannot be null or empty.");
+                Console.WriteLine("Do you want to continue the operation?");
+                Thread.Sleep(1000);
+                Console.WriteLine("1. Yes");
+                Console.WriteLine("2. No");
+
+                optionSelected = Console.ReadLine()!;
+
+                if (optionSelected == "2")
+                {
+                    Console.WriteLine("Operation cancelled.");
+                    return string.Empty;
+                }
+            }
+
+            return fieldValue;
+        }
+    }
+}
