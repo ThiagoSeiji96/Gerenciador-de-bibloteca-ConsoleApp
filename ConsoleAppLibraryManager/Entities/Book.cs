8f0ff6284868921b0ffcde0360e9d1e6c377a8c6
Thiago Seiji Imai Rozner <thiago.rozner@viveo.com.br>
2025-10-21 23:16:56 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppLibraryManager/Entities/Book.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleAppLibraryManager.Entities.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLibraryManager.Entities
+{
+    internal class Book : BaseModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ ConsoleAppLibraryManager/Entities/Borrow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLibraryManager.Entities
+{
+    internal class Borrow
+    {
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartBorrow { get; set; }
+        public DateTime FinishBorrow { get; set; }
+    }
+}
